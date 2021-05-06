1a686c0f7d34d11e2213d59b335b49130f17338f
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-06 11:45:16 +0530

    ADD:Create Simple LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,15 @@
-class LinkedList
-{
-public static void main(String args[])
-{
-System.out.println("welcome to linkedList Program");
-}
-}
+import java.util.LinkedList;
+
+public class LinkedList {
+	
+		public static void main(String args[])
+		{
+			LinkedList<Integer> linkedList = new LinkedList<>();
+			linkedList.add(56);
+			linkedList.add(30);
+			linkedList.add(70);
+			
+			System.out.println(linkedList);
+		}
+	}
+
