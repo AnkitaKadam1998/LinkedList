aef8934bf77369c375ab917543888072fba41e75
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-06 11:57:48 +0530

    ADD:careted Linked List by adding

--- LinkedList.java
+++ LinkedList.java
@@ -1,15 +1,51 @@
 import java.util.LinkedList;
 
-public class LinkedList {
+public class CreateLinkedList {
 	
 		public static void main(String args[])
 		{
-			LinkedList<Integer> linkedList = new LinkedList<>();
-			linkedList.add(56);
-			linkedList.add(30);
-			linkedList.add(70);
+			LinkedList linkedList = new LinkedList<>();
+			linkedList.push(70);
+			linkedList.push(30);
+			linkedList.push(56);
 			
-			System.out.println(linkedList);
+			linkedList.printList();
 		}
 	}
+
+class LinkedList
+{
+	Node head;
+	
+	class Node
+	{
+		int data;
+		Node next;
+		
+		Node(int data)
+		{
+			this.data=data;
+			next = null;
+		}
+	}
+	public void push(int data)
+	{
+		Node newNode = new Node(data);
+		newNode.next=head;
+		head=newNode;
+	}
+	
+	public void printList()
+	{
+		System.out.println("List:");
+		Node temp = head;
+		
+		while(temp != null)
+		{
+			System.out.print(temp.data+ " ");
+			temp = temp.next;
+		}
+	}
+}
+
 
