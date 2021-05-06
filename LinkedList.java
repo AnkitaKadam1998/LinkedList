6ea0f89195156cb27de71b4afe30834d2366d14b
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-06 18:28:03 +0530

    ADD:Insert 30 between 56 and 70

--- LinkedList.java
+++ LinkedList.java
@@ -1,86 +1,76 @@
 package com.LinkedList;
+import com.LinkedList.LinkedList.Node;
+	
+public class insertList {
+		public static void main(String args[])
+		{
+			LinkedList linkedList = new LinkedList();
+			linkedList.append(56);
+			linkedList.append(30);
+			
+			
+			linkedList.insertAfter(linkedList.head.next, 70);
+			linkedList.printList();
+		
+		}
+	}
+	class LinkedList
+	{
+		Node head;
+		
+		class Node
+		{
+			int data;
+			Node next;
+			
+			Node(int data)
+			{
+				this.data = data;
+				next = null;
+			}
+		}
+		
+		public void printList() {
+			System.out.println("List : ");
+			Node temp = head;
+			
+			while(temp != null) {
+				System.out.print(temp.data+ "->");
+				temp = temp.next;
+			}
+		}
+		
+		public void append(int data)
+		{
+			Node newNode = new Node(data);
+			if(head == null)
+			{
+				head = newNode;
+				return;
+			}
+			
+			Node last = head;
+			while(last.next!= null)
+			{
+				last = last.next;
+			}
+			last.next = newNode;
+		}
 
-class Node {
-    int data;
-    Node next;
-};
-
-class LinkedList5 {
-  Node head;
-
-  LinkedList5(){
-    head = null;
-  }
-
-  //Add new element at the end of the list
-  public void push_back(int newElement) {
-    Node newNode = new Node();
-    newNode.data = newElement;
-    newNode.next = null; 
-    if(head == null) {
-      head = newNode;
-    } else {
-      Node temp = new Node();
-      temp = head;
-      while(temp.next != null)
-        temp = temp.next;
-      temp.next = newNode;
-    }    
-  }
-
-  //Delete last node of the list
-  public void pop_back() {
-    if(this.head != null) {
-      if(this.head.next == null) {
-        this.head = null;
-      } else {
-        Node temp = new Node();
-        temp = this.head;
-        while(temp.next.next != null)
-          temp = temp.next;
-        Node lastNode = temp.next;
-        temp.next = null; 
-        lastNode = null;
-      }
-    }
-  }
-    
-  
-
-  //display the content of the list
-  public void PrintList() {
-    Node temp = new Node();
-    temp = this.head;
-    if(temp != null) {
-      System.out.print("The list contains: ");
-      while(temp != null) {
-        System.out.print(temp.data + " -> ");
-        temp = temp.next;
-      }
-      System.out.println();
-    } else {
-      System.out.println("The list is empty.");
-    }
-  }    
-};
+		 public void insertAfter(Node prevNode,int data)
+		 {
+			if(prevNode == null)
+			{
+				System.out.println("previous node should not be null");
+				return;
+			}
+			Node newNode = new Node(data);
+			newNode.next = prevNode.next;
+			prevNode.next =newNode;
+		 	}
+		}
 
 
-public class DeleteLastElement{ 
-  public static void main(String[] args) {
-    LinkedList MyList = new LinkedList();
-
-    
-    MyList.push_back(56);
-    MyList.push_back(30);
-    MyList.push_back(70);
-    
-    MyList.PrintList(); 
-
-    //Delete the first node
-    MyList.pop_back();
-    MyList.PrintList();  
-  }
-}
 
 
 
