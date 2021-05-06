efb52c5edd01c890a17ac9338b254c4d6c35bd11
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-05-06 12:25:06 +0530

    ADD:Delete first element using pop method

--- LinkedList.java
+++ LinkedList.java
@@ -1,80 +1,76 @@
-import java.util.*;
+package com.LinkedList;
 
-public class insertList {
-	
-		public static void main(String args[])
-		{
-			LinkedList linkedList = new LinkedList<>();
-			linkedList.push(70);
-			linkedList.push(30);
-			linkedList.push(56);
-			
-			linkedList.insertAfter(linkedList.head.next,40);
-			linkedList.printList();
-		}
-	}
+class Node {
+    int data;
+    Node next;
+};
 
-class LinkedList
-{
-	Node head;
-	
-	class Node
-	{
-		int data;
-		Node next;
-		
-		Node(int data)
-		{
-			this.data=data;
-			next = null;
-		}
-	}
-	public void push(int data)
-	{
-		Node newNode = new Node(data);
-		newNode.next=head;
-		head=newNode;
-	}
-	
-	public void printList()
-	{
-		System.out.println("List:");
-		Node temp = head;
-		
-		while(temp != null)
-		{
-			System.out.print(temp.data+ " ");
-			temp = temp.next;
-		}
-	}
-	public void append(int data)
-	{
-		Node newNode = new Node(data);
-		if(head == null)
-		{
-			head = newNode;
-			return;
-		}
-		
-		Node last = head;
-		while(last.next!= null)
-		{
-			last = last.next;
-		}
-		last.next = newNode;
-	}
-	public void insertAfter(Node prevNode,int data)
-		{
-			if(prevNode == null)
-			{
-				System.out.println("previous node should not be null");
-				return;
-			}
-			Node newNode = new Node(data);
-			newNode.next = prevNode.next;
-			prevNode.next =newNode;
-		 	}
-		}
+class LinkedList5 {
+  Node head;
+
+  LinkedList5(){
+    head = null;
+  }
+
+  //Add new element at the end of the list
+  public void push_back(int newElement) {
+    Node newNode = new Node();
+    newNode.data = newElement;
+    newNode.next = null; 
+    if(head == null) {
+      head = newNode;
+    } else {
+      Node temp = new Node();
+      temp = head;
+      while(temp.next != null)
+        temp = temp.next;
+      temp.next = newNode;
+    }    
+  }
+
+  //Delete first node of the list
+  public void pop_front() {
+    if(this.head != null) {
+      Node temp = this.head;
+      this.head = this.head.next;
+      temp = null;  
+    }
+  }
+
+  //display the content of the list
+  public void PrintList() {
+    Node temp = new Node();
+    temp = this.head;
+    if(temp != null) {
+      System.out.print("The list contains: ");
+      while(temp != null) {
+        System.out.print(temp.data + " -> ");
+        temp = temp.next;
+      }
+      System.out.println();
+    } else {
+      System.out.println("The list is empty.");
+    }
+  }    
+};
+
+
+public class DeleteFirstElement{ 
+  public static void main(String[] args) {
+    LinkedList MyList = new LinkedList();
+
+    
+    MyList.push_back(56);
+    MyList.push_back(30);
+    MyList.push_back(70);
+    
+    MyList.PrintList(); 
+
+    //Delete the first node
+    MyList.pop_front();
+    MyList.PrintList();  
+  }
+}
 
 
 
